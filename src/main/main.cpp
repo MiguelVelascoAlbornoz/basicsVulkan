a46638e2dbde50e88d751950f3e3dfb93c817074
migue@pMiguel <miguelandresvelas2006@gmail.com>
2026-04-06 21:39:04 +0100

    Add Doxygen configuration and initial HTML documentation structure

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -1,5 +1,15 @@
+/**
+ * @file main.cpp
+ * @brief Main file of the project.
+ */
+
 #include <stdio.h>
 #include <iostream>
+
+
+#ifndef PROJECT_NAME /** @brief .exe file and window name*/
+#define PROJECT_NAME "Unknown"
+#endif
 
 int main() {
     std::cout << PROJECT_NAME << std::endl;
